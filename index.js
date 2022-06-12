4dd8d084cc2869f7269c7bb007725800ccf5ccf7
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-12 19:05:26 +0300

    commit tree 89f8388cfd567b4d68884a9c28bb1e52d6def94a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(sat1 = "roller-skate"){
+    return `This Saturday, I want to ${sat1}!`
+           
+}
+
+function mondayWork(mon = "go to the office"){
+    return `This Monday, I will ${mon}.`
+
+}
+
+let wrapAdjective = function(def="*") {
+    return function(adj="special") {
+          return `You are ${def}${adj}${def}!`
+        }
+}
+
